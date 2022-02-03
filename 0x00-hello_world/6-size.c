15de8b2410e072706a2a1e0f515affb9306acab8
yousseffabid <abidyoussef58@gmail.com>
2022-02-03 05:33:08 -0800

    sizeof()

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	long int longintType;
+	long long int lonlongintType
+	char charType;
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return 0;
+}
