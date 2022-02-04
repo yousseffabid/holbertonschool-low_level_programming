dd66f8f83beac240f8be93578b58eaca7967d993
yousseffabid <abidyoussef58@gmail.com>
2022-02-04 04:35:34 -0800

    loop for

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf(i);
+
+	printf("\n");
+
+	return (0);
+}
