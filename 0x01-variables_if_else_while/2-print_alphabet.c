9d0b1444ee8125e1ac4035154d8381ba08406928
yousseffabid <abidyoussef58@gmail.com>
2022-02-04 03:43:51 -0800

    for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+	printf("\n");
+
+	return (0);
+}
