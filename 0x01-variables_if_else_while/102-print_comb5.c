ff8999d9ef01cad2369ba26a4c8627e83d365495
yousseffabid <abidyoussef58@gmail.com>
2022-02-06 02:23:19 -0800

    4 nest loops

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include<stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int n;
+	int m;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (n = i; n <= '9'; n++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(' ');
+				for (m = j + 1; m <= '9'; m++)
+				{
+					putchar(n);
+					putchar(m);
+					if (m != '9' || n != '9' || i != '9' || j != '8')
+					{
+						putchar(',');
+					}
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
