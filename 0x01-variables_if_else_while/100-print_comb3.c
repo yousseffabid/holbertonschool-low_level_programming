8d3455a6552258aa751f8f45e21dad5f8323349e
yousseffabid <abidyoussef58@gmail.com>
2022-02-06 00:51:53 -0800

    nested loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i + 1 != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
