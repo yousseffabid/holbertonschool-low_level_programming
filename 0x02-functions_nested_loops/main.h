06bc534e75ce243ed7c65f722963e2eaf7de4b1f
yousseffabid <abidyoussef58@gmail.com>
2022-02-08 07:59:02 -0800

    Header created for user functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@
 #include <unistd.h>
 int _putchar(char c);
 void print_alphabet(void);
-int int_islower(int c);
+int _islower(int c);
 void print_alphabet_x10(void);
 int _isalpha(int c);
 int print_sign(int n);
