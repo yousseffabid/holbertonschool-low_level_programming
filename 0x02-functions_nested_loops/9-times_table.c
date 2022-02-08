dd0f0b5f93037f9e5442483ee422fb7a166f9c50
yousseffabid <abidyoussef58@gmail.com>
2022-02-08 13:21:03 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,13 +12,19 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			printf("%d", i * j);
-			if (j != 9)
-				printf(",");
-			if (i * j > 9)
-				printf(" ");
+			if (i == 0)
+			{
+				if (j != 9)
+					printf("%d,  ", i * j);
+				else
+					printf("%d", i * j);
+			}
+			else if (i != 0 && j == 0)
+				printf("%d", i * j);
+			else if (i * j > 9)
+				printf(", %d", i * j);
 			else
-				printf("  ");
+				printf(",  %d", i * j);
 		}
 		printf("\n");
 	}
