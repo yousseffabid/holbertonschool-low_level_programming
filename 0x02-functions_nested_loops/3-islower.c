29e82bcf6a86274dda2c4faaccb5a0966cd67de0
yousseffabid <abidyoussef58@gmail.com>
2022-02-08 08:10:21 -0800

    Header created for user functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - check if the character is lowercase
- *
+ * @c: The character the check if it's a lowercase or uppercase
  * Return: On success 1.
  * On error, 0 is returned, the character is uppercase
  */
 
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
