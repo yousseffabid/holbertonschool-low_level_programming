15a4dd9cd86ec1d57cde334ac56ecf2269e69e5b
yousseffabid <abidyoussef58@gmail.com>
2022-02-08 06:11:50 -0800

    print string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7 ; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+	return (0);
+}
