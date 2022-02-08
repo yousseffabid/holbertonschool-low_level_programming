9f99c7d44f09783d86dea4abb1ee80088a33c556
yousseffabid <abidyoussef58@gmail.com>
2022-02-08 04:15:39 -0800

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include "main.h"
-#include  <unistd.h>
 /**
  * print_alphabet - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet(void)
 {
