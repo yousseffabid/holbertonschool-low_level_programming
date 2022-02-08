52161ac9c5fe4bf78e6469f9c974945e769bf87c
yousseffabid <abidyoussef58@gmail.com>
2022-02-08 12:22:06 -0800

    sum

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,27 @@
 void print_to_98(int n)
 {
 	int i;
-
-	for (i = n; i <= 98; i++)
+	
+	if (n > 98)
 	{
-		printf("%d", i);
-		if (i != 98)
-			printf(", ");
+		 for (i = n; i>= 98; i--)
+                {
+                        printf("%d", i);
+                        if (i != 98)
+                                printf(", ");
+
+                }
+		printf("\n");
 	}
-	printf("\n");
+
+	else
+	{
+		for (i = n; i <= 98; i++)
+        	{
+                	printf("%d", i);
+                	if (i != 98)
+                        	printf(", ");
+	        }
+		printf("\n"):
+	}
 }
