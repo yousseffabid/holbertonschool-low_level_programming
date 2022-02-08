950772c0b0d0ebe98d8b7e3aebcf96a05f727046
yousseffabid <abidyoussef58@gmail.com>
2022-02-08 04:36:37 -0800

    my header that contains functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
+
 /**
- * print_alphabet - writes the character c to stdout
- * @c: The character to print
- *
+ * print_alphabet - writes alpahbets and prints a new line
+ * Return: On success void.
  */
 void print_alphabet(void)
 {
