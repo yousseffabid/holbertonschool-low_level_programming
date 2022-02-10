d4195d1c6e0f3008d96ef2ac39368f8afcc2d409
yousseffabid <abidyoussef58@gmail.com>
2022-02-10 10:05:14 -0800

    prints any integer using putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - prints an integer using putchar
+ * @n: the number to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i = 10;
+	int j;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	if (n <= 9 && n > 0)
+	{
+		_putchar(n + 48);
+	}
+	else
+	{
+		while (n / i != 0)
+		{
+			i *= 10;
+		}
+		while (i > 1)
+		{
+			i /= 10;
+			j = (abs(n) / i) % 10;
+			_putchar(j + 48);
+		}
+		_putchar('\n');
+	}
+}
