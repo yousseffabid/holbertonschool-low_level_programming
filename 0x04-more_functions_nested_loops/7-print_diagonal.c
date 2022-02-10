4223f6fca82c98be81f12fc21be82db89dc6b50e
yousseffabid <abidyoussef58@gmail.com>
2022-02-10 03:04:06 -0800

    prints '\'

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a '\'
+ * @n: number of times to write the '\'
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i < n; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\')
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
