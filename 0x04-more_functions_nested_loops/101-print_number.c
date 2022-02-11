3ccd6341bd9bef88d3c8e960fc5264e64c05d97c
yousseffabid <abidyoussef58@gmail.com>
2022-02-11 08:03:40 -0800

    print numbers using putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,16 @@
  */
 void print_number(int n)
 {
+	int i;
+
+	i = 10;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-
-	if (n / 10)
+	if (n / 10 != 0)
 	{
 		print_number(n / 10);
 	}
