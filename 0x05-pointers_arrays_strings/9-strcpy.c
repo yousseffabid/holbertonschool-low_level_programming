d629aeea99681460b45d32c839cbcf238c1df0f3
yousseffabid <abidyoussef58@gmail.com>
2022-02-14 09:13:20 -0800

    copy a string to another string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,9 +13,9 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	if (dest[i + 1] != '\0')
+	if (dest[i] != '\0')
 	{
-		dest[i + 1] = '\0';
+		dest[i] = '\0';
 	}
 	return (dest);
 }
