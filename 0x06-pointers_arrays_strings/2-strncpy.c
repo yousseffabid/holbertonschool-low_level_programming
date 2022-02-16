2aa13e520129c09828c586ccc6485f0be1d0d7cd
yousseffabid <abidyoussef58@gmail.com>
2022-02-16 03:17:54 -0800

    copy n characters of a string to another srting

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+/**
+ * _strncpy - copy a specific number of characters to the destination string
+ * @dest: destination string
+ * @src: source string
+ * @n: numbers of characters to append to the dest string
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (dest[i] != '\0')
+		dest[i++] = '\0';
+	return (dest);
+}
