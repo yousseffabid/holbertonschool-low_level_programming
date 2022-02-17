273e0fb8c399d430d15beed44e07d941974d9d72
yousseffabid <abidyoussef58@gmail.com>
2022-02-17 03:46:51 -0800

    compares strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,24 +8,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int result;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] < s2[i])
+		if (s1[i] != s2[i])
 		{
-			result = s1[i] - s2[i];
-			break;
-		}
-		else if (s1[i] > s2[i])
-		{
-			result = s1[i] - s2[i];
-			break;
-		}
-		else
-		{
-			result = 0;
+			return (s1[i] - s2[i]);
 		}
 	}
-	return (result);
+	return (0);
 }
