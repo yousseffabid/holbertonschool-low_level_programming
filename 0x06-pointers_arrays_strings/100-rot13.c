09805a592695ef5b0c6a1207926bde7c31a677fe
yousseffabid <abidyoussef58@gmail.com>
2022-02-18 00:34:26 -0800

    encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 -  encodes a string into rot13
+ * @s: string to be encoded
+ * Return: encccoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char alphabets[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char encoded_alphabets[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alphabets[j])
+			{
+				s[i] = encoded_alphabets[j];
+			}
+		}
+	}
+	return (s);
+}
