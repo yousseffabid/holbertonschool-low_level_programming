af184c5fc342b287f23f47922d086f6ffd9d814d
yousseffabid <abidyoussef58@gmail.com>
2022-02-21 01:08:02 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset -  fills the first n bytes of the memory area with the constant byte b
+ * @s: pointer to string
+ * @b: character to be copied to the string
+ * @n: number of bytes
+ * Return: pointer to string.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+}
