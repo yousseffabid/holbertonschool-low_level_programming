589b79e926d4b113212582c6a9cb60a103f8957e
yousseffabid <abidyoussef58@gmail.com>
2022-02-21 11:37:36 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,20 +7,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    while (*haystack)
-    {
-        char *Begin = haystack;
-        char *pattern = needle;
+	while (*haystack)
+	{
+		char *Begin = haystack;
+		char *pattern = needle;
 
-        while (*haystack && *pattern && *haystack == *pattern)
-        {
-            haystack++;
-            pattern++;
-        }
-        if (!*pattern)
-            return (Begin);
+		while (*haystack && *pattern && *haystack == *pattern)
+		{
+			haystack++;
+			pattern++;
+		}
+		if (!*pattern)
+			return (Begin);
 
-        haystack = Begin + 1;
-    }
-    return (0);
+		haystack = Begin + 1;
+	}
+	return ((void *)0);
 }
