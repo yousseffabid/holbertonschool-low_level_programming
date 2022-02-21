0218ab39ef15683676b376fb80bbf85370a57369
yousseffabid <abidyoussef58@gmail.com>
2022-02-21 11:33:23 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,33 +7,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-		int i, j, k, l, present;
+    while (*haystack)
+    {
+        char *Begin = haystack;
+        char *pattern = needle;
 
-		j = 0;
-		i = 0;
-		present = 1;
-		while (haystack[i] != '\0')
-		{
-			if (needle[j] == haystack[i])
-			{
-				k = i + 1;
-				l = j + 1;
-				while (needle[l] != '\0')
-				{
-					if (needle[l] != haystack[k])
-					{
-						present = 0;
-						break;
-					}
-					else
-						present = 1;
-					k++;
-					l++;
-				}
-				if (needle[l] == '\0' && present == 1)
-				return (needle);
-			}
-			i++;
-		}
-		return (0);
+        while (*haystack && *pattern && *haystack == *pattern)
+        {
+            haystack++;
+            pattern++;
+        }
+        if (!*pattern)
+            return (Begin);
+
+        haystack = Begin + 1;
+    }
+    return (0);
 }
