ff948f27a0053957c4a1ee53cb7bf62a89bf6e4c
yousseffabid <abidyoussef58@gmail.com>
2022-02-21 01:55:38 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strchr - pointer to the 1st occurrence of the char c in s
+ * @s: string
+ * @c: character to check its occurrence
+ * Return: pointer to string.
+ */
+char *_strchr(char *s, char c)
+{
+	char *result;
+	int i, j;
+
+	j = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			while (s[i] != '\0')
+			{
+				result[j] = s[i];
+				i++;
+				j++;
+			}
+			break;
+		}
+	}
+	if (j == 0)
+		return (NULL);
+	result[j] = '\0';
+	return (result);
+}
