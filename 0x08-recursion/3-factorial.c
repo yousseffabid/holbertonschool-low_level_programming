8c2f1ffac795b8ef2fea098a717ffe08ea76d995
yousseffabid <abidyoussef58@gmail.com>
2022-02-23 04:30:11 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - factorial of a given number.
+ * @n: Pointer to a string
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
