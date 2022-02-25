fd6cf8c8e33e10d98bf613e589fd46a6b3b1eb0a
yousseffabid <abidyoussef58@gmail.com>
2022-02-25 01:41:30 -0800

    display name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: number of arguments aupplied to program
+ * @argv: array that contains the arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
