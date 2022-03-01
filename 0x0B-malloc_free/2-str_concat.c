40326a38799a547db0a2148b7997095a9a7fec73
yousseffabid <abidyoussef58@gmail.com>
2022-03-01 03:04:22 -0800

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,10 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *ptr;
 
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	;
 	for (j = 0; s2[j] != '\0'; j++)
