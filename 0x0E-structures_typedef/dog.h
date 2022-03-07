3c1dcf67c8389eef6a1a5101c8f0cd8b34bb08b4
yousseffabid <abidyoussef58@gmail.com>
2022-03-07 08:06:12 -0800

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct that groups the characteristics of the dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ * Description: This struct has the data of my_dog
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
