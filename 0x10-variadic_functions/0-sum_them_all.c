00a4a5ec1498b8a1dbace9dda04a62d3152f8300
yousseffabid <abidyoussef58@gmail.com>
2022-03-10 02:04:59 -0800

    sum of number

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all its parameters
+ * @n: number of parameters
+ * @...: rest of parameters
+ * Return: int, sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	var_list ap;
+	int sum;
+	unsigned int i;
+
+	sum = 0;
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
