e62a8529b3bd9165eba827b54ab9e4861ac39d8e
yousseffabid <abidyoussef58@gmail.com>
2022-03-17 04:21:16 -0700

    display number of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+
+#include "lists.h"
+/**
+ * list_len - returns number of elements in the linked list
+ * @h: head of list
+ * Return: size_t number of elements in the linked list
+ */
+size_t list_len(const list_t *h);
+{
+	size_t numberof_nodes;
+	const list_t *tmp;
+
+	tmp = NULL;
+	if (h != NULL)
+		tmp = h;
+
+	numberof_nodes = 0;
+	while (tmp->next != NULL)
+	{
+		numberof_nodes++;
+		tmp = tmp->next;
+	}
+	return (numberof_nodes);
+}
