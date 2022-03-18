eb2cf649a59889848f8aee8c1f29828a25a5ecca
yousseffabid <abidyoussef58@gmail.com>
2022-03-18 03:55:28 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,6 +8,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *tmp;
+
 	if (old_size == new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
@@ -15,12 +17,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL || new_size > old_size)
+	if (ptr == NULL)
 	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
+		tmp = malloc(new_size);
+		if (tmp == NULL)
 			return (NULL);
-		return (ptr);
+		free(ptr);
+		return (tmp);
+	}
+	if (new_size > old_size)
+	{
+		tmp = malloc(new_size);
+		if (tmp == NULL)
+			return (NULL);
+		free(ptr);
+		return (tmp);
 	}
 	return (ptr);
 }
