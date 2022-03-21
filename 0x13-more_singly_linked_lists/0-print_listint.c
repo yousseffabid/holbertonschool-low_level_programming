89014661af93f8004a68bbb1fda84470aab7a449
yousseffabid <abidyoussef58@gmail.com>
2022-03-21 02:56:16 -0700

    print value of nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a list_t list
+ * @h: head of the list
+ * Return: number of nodes printed
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
