700760d5db96ccf7c6ff06d5894eee0d1ac3e90a
yousseffabid <abidyoussef58@gmail.com>
2022-03-21 05:05:35 -0700

    add node in the 1st place

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_nodeint - returns number of elements in the linked list
+ * @head: pointer to the head of list
+ * @n: value data
+ * Return: address of the new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);	
+	new_node->next = *head;
+	new_node->n = n;
+	*head = new_node;
+
+	return (*head);
+}
