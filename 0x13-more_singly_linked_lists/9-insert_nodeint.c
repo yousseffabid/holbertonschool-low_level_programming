42facfc94824d01c803ecb18905bea80e0ec11cb
yousseffabid <abidyoussef58@gmail.com>
2022-03-22 21:09:36 +0100

    insert at ith position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-			return (NULL);
+		return (NULL);
 	new_node->n = n;
 	if (idx == 0)
 	{
