a8541738a3000d63f74ccf074ff0dc84385880e9
yousseffabid <abidyoussef58@gmail.com>
2022-03-22 18:38:37 +0100

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,24 +1,17 @@
 #include "lists.h"
 /**
- * add_nodeint_end - add a new node at the end of the list
+ * free_listint - free the list
  * @head: pointer head of the list
- * @n: string of the new node
- * Return: address of the inserted node
+ * Return: void
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+void free_listint(listint_t *head)
 {
-	listint_t *tmp, *new_node;
+	listint_t *tmp;
 
-	new_node = malloc(sizeof(lisint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-	tmp = *head;
-
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-
-	tmp->next = new_node;
-	return (new_node);
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
