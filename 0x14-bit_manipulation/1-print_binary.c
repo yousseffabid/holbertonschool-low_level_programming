6d71d34a7a7c7cf9c58594f118b86a747d6b2e15
yousseffabid <abidyoussef58@gmail.com>
2022-03-24 14:38:01 +0100

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_binary - convert ul to binary
+ * @n: number
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long check, i, multi_of_two;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (multi_of_two = 1; (multi_of_two * 2) <= n; multi_of_two *= 2)
+	;
+	check = 0;
+	for (i = 0; ; i++)
+	{
+		check = multi_of_two >> i;
+
+		if (check == 0)
+			break;
+
+		if ((n & check) > 0)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
