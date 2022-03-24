b83039f155ac7a3f539245d78fda52fc660839af
yousseffabid <abidyoussef58@gmail.com>
2022-03-24 17:22:50 +0100

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,64 +1,29 @@
 #include "main.h"
 /**
- * power - a to the power of b
- * @a: base
- * @b: exponent
- * Return: unsigned int
- */
-unsigned int power(unsigned int a, unsigned int b)
-{
-	unsigned int i, base;
-
-	if (b == 0)
-		return (1);
-	i = 1;
-	base = a;
-	while (i < b)
-	{
-		a *= base;
-		i++;
-	}
-	return (a);
-}
-
-/**
- * _strlen - returns length of string
- * @b: pointer to const char
- * Return: unsigned int
- */
-unsigned int _strlen(const char *b)
-{
-	unsigned int i;
-
-	i = 0;
-	for (; *b; b++)
-		i++;
-	return (i);
-}
-
-/**
  * binary_to_uint -converts binary to an uns int
  * @b: pointer to char
  * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, len, uint;
+	unsigned int uint = 0;
 
-	if (b == NULL || *b == '\0')
+	if (b == NULL)
 		return (0);
-	uint = 0;
-	len = _strlen(b) - 1;
-	for (i = 0; b[i] != '\0'; i++)
+
+	while (*b)
 	{
-		if (b[i] >= '0' && b[i] <= '9')
-		{
-			if ((b[i] - '0') == 1)
-				uint += power(2, len);
-			len--;
-		}
+		if (*b == '1')
+			uint = (uint << 1) | 1;
+
+		else if (*b == '0')
+			uint <<= 1;
+
 		else
 			return (0);
+
+		b++;
 	}
+
 	return (uint);
 }
