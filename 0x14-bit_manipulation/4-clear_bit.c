4b9db3664a60098981d66cc22474a1a385c0b51e
yousseffabid <abidyoussef58@gmail.com>
2022-03-24 20:34:57 +0100

    set bit to zero

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * set_bit - get the value of the nth bit
+ * @n: pointer to unsigned number
+ * @index: index of the bit
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = 1 << index;
+
+	if (index > 63)
+		return (-1);
+
+	*n = (*n) & mask;
+
+	if (*n)
+		return (1);
+	else
+		return (-1);
+}
