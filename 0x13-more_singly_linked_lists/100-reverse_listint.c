3941e734fb7c999388b834cffb3ffbdf0529a54b
yousseffabid <abidyoussef58@gmail.com>
2022-03-25 22:28:38 +0100

    singly linked liists

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * reverse - reverse list
+ * @ptr: pointer to head of the list
+ * Return: pointer to listint_t
+ */
+listint_t *reverse(listint_t *ptr)
+{
+	listint_t *tmp;
+
+	if (ptr->next == NULL)
+		return (ptr);
+
+	tmp = reverse(ptr->next);
+	(ptr->next)->next = ptr;
+
+	return (ptr);
+
+}
+/**
+ * reverse_listint - reverse list
+ * @head: pointer to pointer to head of the list
+ * Return: pointer to listint_t
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ptr;
+
+	ptr = *head;
+	*head = reverse(ptr);
+
+	return (*head);
+}
