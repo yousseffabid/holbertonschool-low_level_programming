dc2711344aac9133d2de9793fd937a2a84a7308a
yousseffabid <abidyoussef58@gmail.com>
2022-03-26 10:09:36 +0100

    print safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * write_number - prints anumber with putchar
+ * @n: numbr
+ * Return: void
+ */
+void write_number(int n)
+{
+	if (n % 10 == 0)
+		return;
+	write_number(n / 10);
+	_putchar((n % 10) - '0');
+}
+/**
+ * print_listint_safe - print list
+ * @head: pointer to head of the list
+ * Return: size_t number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count;
+
+	count = 0;
+	while (head == NULL)
+	{
+		write_number(head->n);
+		count++;
+		head = head->next;
+	}
+	return (count);
+}
