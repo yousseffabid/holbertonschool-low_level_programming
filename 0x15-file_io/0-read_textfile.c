9275c576ce0a4e89a704e2d6f814760edb9ed83b
yousseffabid <abidyoussef58@gmail.com>
2022-03-28 12:13:05 +0100

    file descriptors

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and prints it to stdout
+ * @filename: filename
+ * @letter: number of letters to read from text
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, w, r;
+	char *buffer;
+
+	if (filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	r = read(fd, buffer, letters);
+	buffer[letters] = '\0';
+
+	w = write(1, buffer, letters);
+	if (w)
+		return (0);
+
+	close(fd);
+
+	return (r);
+}
