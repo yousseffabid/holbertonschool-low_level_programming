0f3a7590333ee132f92e87255b39f8f21ebf2b15
yousseffabid <abidyoussef58@gmail.com>
2022-03-28 13:42:50 +0100

    file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- * read_textfile -  reads a text file and prints it to stdout
+ * append_text_to_file - append text to file
  * @filename: filename
- * @letters: number of letters to read from text
- * Return: ssize_t
+ * @text_content: text
+ * Return: int
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, w;
+	int fd, w;
 
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
-    if (text_content != NULL)
-        w = write(fd, text_content, strlen(text_content));
-    else
-        w = write(fd, 0 , 0);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		w = write(fd, text_content, strlen(text_content));
+	else
+		w = write(fd, 0, 0);
 
-    if (w == -1)
-        return (-1);
+	if (w == -1)
+		return (-1);
 
-    return (1);
+	close(fd);
+	return (1);
 }
