26c656cfc3d22555f589f460101602704f2fff94
yousseffabid <abidyoussef58@gmail.com>
2022-03-28 12:47:40 +0100

    file descriptor

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,14 +14,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char*) * letters);
 	if (buffer == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 
 	r = read(fd, buffer, letters);
 
