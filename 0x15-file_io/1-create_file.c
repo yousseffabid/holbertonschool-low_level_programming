19f71790ccc06de9df0a784b8b3f6dc6128dd595
yousseffabid <abidyoussef58@gmail.com>
2022-03-28 13:21:07 +0100

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * read_textfile -  reads a text file and prints it to stdout
+ * create_file -  creates a file
  * @filename: filename
- * @letters: number of letters to read from text
- * Return: ssize_t
+ * @text_content: text
+ * Return: int
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -13,8 +13,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-  if (fd == -1)
-      return (-1);
+	if (fd == -1)
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -24,12 +24,12 @@ int create_file(const char *filename, char *text_content)
 
 		w = write(fd, text_content, i);
 	}
-  else
-      w = write(fd, 0, 0);
+	else
+	  w = write(fd, 0, 0);
 
-  if (w == -1)
-      return (-1);
+	if (w == -1)
+		return (-1);
 
-  close (fd);
+	close(fd);
 	return (1);
 }
