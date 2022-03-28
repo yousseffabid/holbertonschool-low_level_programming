b8b31164a55a58b4a29cd704fa4870a324f2961e
yousseffabid <abidyoussef58@gmail.com>
2022-03-28 13:30:40 +0100

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,22 +8,16 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, w, fd;
+	int w, fd;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
-	{
-		i = 0;
-		while (*text_content)
-			i++;
-
-		w = write(fd, text_content, i);
-	}
+		w = write(fd, text_content, strlen(text_content));
 	else
 	  w = write(fd, 0, 0);
 
