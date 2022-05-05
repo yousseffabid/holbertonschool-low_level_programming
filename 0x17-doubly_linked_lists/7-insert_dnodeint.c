685da86f1f28c98a1f99395dd117312025f61079
yousseffabid <abidyoussef58@gmail.com>
2022-05-05 15:21:38 +0100

    doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,10 +35,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (head_ref)
 	{
 		if (head_ref->prev != NULL)
-		{
 			head_ref->prev->next = new_node;
-			new_node->prev = head_ref->prev;
-		}
+		new_node->prev = head_ref->prev;
 		head_ref->prev = new_node;
 	}
 	else
