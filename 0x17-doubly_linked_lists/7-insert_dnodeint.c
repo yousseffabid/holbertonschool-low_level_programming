ba7adb4f2cdeb5b03d2364b6e5ab9cb25f74cb81
yousseffabid <abidyoussef58@gmail.com>
2022-05-05 22:13:09 +0100

    doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,69 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - inserts a node at index
- * @h: head of list
- * @idx: index of node
- * @n: data of the node
- * Return: pointer to the inserted node
+ * insert_dnodeint_at_index - inserts node at index
+ *
+ * @h: head
+ * @idx: index
+ * @n: data
+ * Return: ptr to new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *head_ref;
-	unsigned int counter;
+	dlistint_t *newNode, *ptr;
+	unsigned int i = 0, len;
+
+	if (!h)
+		return (NULL);
+	len = dlistint_len(*h);
+	if (idx > len)
+		return (NULL);
+
+	if (*h == NULL && idx == 0)
+		return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (idx >= len)
+		return (add_dnodeint_end(h, n));
+
+	ptr = (*h);
+	while (ptr)
+	{
+		if (i == idx - 1)
+			break;
+		ptr = ptr->next;
+		i++;
+	}
+	if (ptr)
+	{
+		newNode = malloc(sizeof(dlistint_t));
+		if (newNode == NULL)
+			return (NULL);
+		newNode->n = n;
+		newNode->prev = ptr;
+		newNode->next = ptr->next;
+		ptr->next->prev = newNode;
+		ptr->next = newNode;
+	}
+
+	return (newNode);
+}
+/**
+ * dlistint_len - lenght of Doubly linked list
+ * @h: head of list
+ * Return: Number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	unsigned int count = 0;
 
 	if (h == NULL)
-		return (NULL);
-	head_ref = *h;
-
-	counter = 0;
-	while (head_ref)
+		return (0);
+	ptr = h;
+	while (ptr != NULL)
 	{
-		if (idx == counter)
-			break;
-		head_ref = head_ref->next;
-		counter++;
+		count++;
+		ptr = ptr->next;
 	}
-	if (idx != counter)
-		return (NULL);
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = head_ref;
-
-	if (head_ref)
-	{
-		if (head_ref->prev != NULL)
-			head_ref->prev->next = new_node;
-		new_node->prev = head_ref->prev;
-		head_ref->prev = new_node;
-	}
-	else
-	{
-		new_node->prev = NULL;
-		*h = new_node;
-	}
-	return (new_node);
+	return (count);
 }
