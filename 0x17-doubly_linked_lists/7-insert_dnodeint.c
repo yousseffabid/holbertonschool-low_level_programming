71a20fb8f61176c90e4014863816900f7741499d
yousseffabid <abidyoussef58@gmail.com>
2022-05-06 10:05:42 +0100

    doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,60 +10,33 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newNode, *ptr;
-	unsigned int i = 0, len;
-
-	if (!h)
-		return (NULL);
-	len = dlistint_len(*h);
-	if (idx > len)
-		return (NULL);
-
-	if (*h == NULL && idx == 0)
-		return (add_dnodeint(h, n));
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (idx >= len)
-		return (add_dnodeint_end(h, n));
-
-	ptr = (*h);
-	while (ptr)
-	{
-		if (i == idx - 1)
-			break;
-		ptr = ptr->next;
-		i++;
-	}
-	if (ptr)
-	{
-		newNode = malloc(sizeof(dlistint_t));
-		if (newNode == NULL)
-			return (NULL);
-		newNode->n = n;
-		newNode->prev = ptr;
-		newNode->next = ptr->next;
-		ptr->next->prev = newNode;
-		ptr->next = newNode;
-	}
-
-	return (newNode);
-}
-/**
- * dlistint_len - lenght of Doubly linked list
- * @h: head of list
- * Return: Number of elements
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	const dlistint_t *ptr;
-	unsigned int count = 0;
+	unsigned int counter;
 
 	if (h == NULL)
-		return (0);
-	ptr = h;
-	while (ptr != NULL)
+		return (NULL);
+	counter = 0;
+	ptr = *h;
+	while (ptr)
 	{
-		count++;
+		if (counter == idx)
+			break;
+		counter++;
 		ptr = ptr->next;
 	}
-	return (count);
+	if (counter != idx)
+		return (NULL);
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = ptr;
+	newNode->prev = NULL;
+	if (ptr->prev != NULL)
+	{
+		newNode->prev = ptr->prev;
+		(ptr->prev)->next = newNode;
+		ptr->prev = newNode;
+	}
+	return (newNode);
 }
+
